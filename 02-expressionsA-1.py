c4b808cbbe0bab20a4195aa11e1779945854d99e
NagashLL <konrad.lipiec@student.nhlstenden.com>
2023-03-15 21:00:27 +0700

    testing order of expressions in python

---
+++ 02-expressionsA-1.py
@@ -0,0 +1,6 @@
+alpha = 35.0
+beta= 12.50
+c = alpha * beta
+print (c)
+#naming variable has to be concise and easy to read to help with understanding and making changes down the line!
+# = is taken as an arrow, so = means that <- ()
