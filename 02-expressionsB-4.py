0feb81303322d5db34c5093344405dbbd49cfdfb
NagashLL <konrad.lipiec@student.nhlstenden.com>
2023-03-16 15:18:01 +0700

    02 files leftovers

---
+++ 02-expressionsB-4.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+print('Nice to meet you', name)
+#                       ^ this is important . use a comma to separate a string and a variable call
+
+# function asks the user the user for input( here ) and then stores that input as a variable
