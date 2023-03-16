3f6de8d9c4da324317f3a87d40649c76ceb10416
NagashLL <konrad.lipiec@student.nhlstenden.com>
2023-03-16 15:17:15 +0700

    finished chapter 2. added the python scripts used

---
+++ 02-EU-US-floorCalculator.py
@@ -0,0 +1,6 @@
+input = input('Hi. I see that you are in Europe. What is the floor you are on?')
+#asks for input, gets a !string!
+usfloor = ( int(input) + 1 )
+#converts the string to a integer
+print ('In the US, you would be on floor', usfloor)
+#gives the result as a string, on which us floor you are on right now
